729a1f5c2997a73307ac3fbec7ac856f8fd0c12f
Alexander <nowordforfree@gmail.com>
2015-07-31 18:21:39 +0300

    Working little game with 3 levels

--- js/game.js
+++ js/game.js
@@ -7,22 +7,20 @@ function Game() {
     this.pass = 0;
     this.fail = 0;
     this.questionsAnswered = 0;
+    this.level = 1;
     this.totalTime = 0;
 }
 
 Game.prototype.start = function(level) {
-    if (!level) {
-        level = 1;
-    }
     this.ask();
 }
 
 Game.prototype.countdownStart = function() {
     this.startTime = performance.now();
     var self = this;
-    $('.pointer').css('left', '0%');
-    $('.pointer').animate({
-        left: '98%'
+    $('.pointer:not(.runner)').css('left', '0%');
+    $('.pointer:not(.runner)').animate({
+        left: '100%'
     }, {
         duration: 10000,
         easing: 'linear',
@@ -30,47 +28,116 @@ Game.prototype.countdownStart = function() {
             self.countdownStop();
             $('.circle.generic').off('click');
             self.fail++;
-            alert('Too long');
+            self.questionsAnswered++;
+            self.end();
         }
     });
 }
 
 Game.prototype.countdownStop = function() {
     var endTime = performance.now();
-    $('.pointer').stop();
+    $('.pointer:not(.runner)').stop();
     $('.circle.generic').off('click');
     this.totalTime += endTime - this.startTime;
 }
 
 Game.prototype.end = function() {
-    if (this.questionsAnswered < 20) {
+    if (this.level < 3 && this.questionsAnswered < 20) {
+        this.ask();
+    } else if (this.level == 3 && this.questionsAnswered < 10) {
         this.ask();
     } else {
         $('#game').hide();
         $('#results').show();
-        var summary = $('#results table');
-        $(summary).find('tr:nth-child(1) td:nth-child(2)').text(this.pass);
-        $(summary).find('tr:nth-child(2) td:nth-child(2)').text(this.fail);
-        $(summary).find('tr:nth-child(3) td:nth-child(2)').text(Math.round(this.totalTime) / 1000 + ' sec');
+        var summary = $('#results table tbody');
+        $(summary).append('<tr><td colspan="2" class="thead">Level ' + this.level + ' score</td></tr>');
+        $(summary).append('<tr><td>Correct answers:</td><td>' + this.pass + '</td></tr>');
+        $(summary).append('<tr><td>Incorrect answers:</td><td>' + this.fail + '</td></tr>');
+        $(summary).append('<tr><td>Time spend:</td><td>' + Math.round(this.totalTime) / 1000 + ' sec</td></tr>');
+        this.pass = 0;
+        this.fail = 0;
+        this.questionsAnswered = 0;
+        this.totalTime = 0;
+        if (this.level < 3) {
+            this.level++;
+            var next = $('<div><span>Ready for next level?</span><br></div>');
+            var btnnext = $('<input type="button" value="    Go!    ">');
+            var self = this;
+            $(next).append(btnnext);
+            $(btnnext).click(function () {
+                $(next).remove();
+                $('#results').hide();
+                $('#game').show();
+                self.ask();
+            });
+            $('#results').append(next);
+        }
     }
 }
 
 Game.prototype.ask = function() {
     this.countdownStart();
-    $('.circle.asked').remove();
-    var color = this.colors[Math.round(Math.random() * this.colors.length)];
-    $('#question').append('<div class="' + color + ' circle asked">' + color + '</div>');
+    $('.asked').remove();
+    var color;
+    var randIndex = Math.floor(Math.random() * this.colors.length);
+    var color = this.colors[randIndex];
+    if (this.level == 1) {
+        $('#question').append('<h1 class="asked ' + color + '">' + color + '</h1>');
+    }
+    if (this.level == 2) {
+        var color2;
+        var rand2 = Math.floor(Math.random() * this.colors.length);
+        while (rand2 == randIndex) {
+            rand2 = Math.floor(Math.random() * this.colors.length);
+        }
+        color2 = this.colors[rand2];
+        $('#question').append('<h1 class="asked ' + color2 + '">' + color + '</h1>');
+    }
+    if (this.level == 3) {
+        var color2;
+        var nextword = '';
+        for (var i = 0; i < color.length; i++) {
+            var rand2 = Math.floor(Math.random() * this.colors.length);
+            while (rand2 == randIndex) {
+                rand2 = Math.floor(Math.random() * this.colors.length);
+            }
+            color2 = this.colors[rand2];
+            nextword += '<span class="' + color2 + '">' + color[i] + '</span>';
+        }
+        $('#question').append('<h1 class="asked">' + nextword + '</h1>');
+    }
 
     var self = this;
-
+    var response = true,
+        clicks = 0;
     $('.circle.generic').click(function() {
-        self.countdownStop();
-        if ($(this).text().trim().toLowerCase() == $('.circle.asked').text()) {
-            self.pass++;
+        if (self.level < 3) {
+            self.countdownStop();
+            if ($('#question .asked').attr('class').indexOf($(this).text().trim().toLowerCase()) > 0) {
+                self.pass++;
+            } else {
+                self.fail++;
+            }
+            self.questionsAnswered++;
+            self.end();
         } else {
-            self.fail++;
+            if (clicks < $('#question .asked').text().length - 1) {
+                if ($('#question .asked span:eq(' + clicks + ')').attr('class').indexOf($(this).text().trim().toLowerCase()) < 0) {
+                    if (response) {
+                        response = false;
+                    }
+                }
+                clicks++;
+            } else {
+                self.countdownStop();
+                if (response) {
+                    self.pass++;
+                } else {
+                    self.fail++;
+                }
+                self.questionsAnswered++;
+                self.end();
+            }
         }
-        self.questionsAnswered++;
-        self.end();
     })
 }
